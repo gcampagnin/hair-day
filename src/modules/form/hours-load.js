1a5d3f4bf5092a72682c3492f1428bef4067306c
Gustavo Campagnin <gcampagnin@gmail.com>
2024-05-21 17:20:26 -0300

    blocking scheduled schedules

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -4,20 +4,25 @@ import { hoursClick } from "./hours-click.js"
 
 const hours = document.getElementById("hours")
 
-export function hoursLoad({ date }) {
+export function hoursLoad({ date, dailySchedules }) {
 	// Limpa a lista de horário.
 	hours.innerHTML = ""
+
+	// Obtém a lista de horários ocupados.
+	const unavailableHours = dailySchedules.map((schedule) => dayjs(schedule.when).format("HH:mm"))
 
 	const opening = openingHours.map((hour) => {
 		// Recuperar somente a hora e minutos.
 		const [scheduleHour, scheduleMinute] = hour.split(":")
 
 		// Adicionar a hora na data e verifica se está no passado.
-		const isHourPast = dayjs(date).hour(scheduleHour).minute(scheduleMinute).isAfter(dayjs())
+		const isHourPast = dayjs(date).hour(scheduleHour).minute(scheduleMinute).isBefore(dayjs())
+
+		const available = !unavailableHours.includes(hour) && !isHourPast
 
 		return {
 			hour,
-			available: isHourPast,
+			available
 		}
 	})
 
